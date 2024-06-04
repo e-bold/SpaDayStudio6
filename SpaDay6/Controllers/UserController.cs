d631434c11a174b6f664227968ffb21faf533cda
e-bold <enkhmunkh@att.net>
2024-06-04 15:34:36 -0500

    Completed Studio for chapter14

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SpaDay6;
+
+public class UserController : Controller
+{
+
+[HttpGet("/user/add")]
+public IActionResult Add()
+{
+    return View("Add");
+}
+
+
+
+[HttpPost("/user/add")]
+public IActionResult SubmitAddUserForm(User newUser, string verify) 
+{
+    if(verify == newUser.Password)
+    {
+        ViewBag.name = newUser.Username;
+        return View("Index");
+    }
+    else
+    {
+        ViewBag.user = newUser.Username;
+        ViewBag.email =newUser.Email;
+        ViewBag.error = "Error. Form needs to be submitted properly. Please Check Password";
+        return View("Add");
+    }
+
+
+
+
+      // add form submission handling code here
+
+}
+}
+
